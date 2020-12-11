1598efcec4a0bf40b116aaebe153cd278f134127
jvidal86 <jvidal86@xtec.cat>
2020-12-11 17:16:11 +0100

    aa

--- 02_Threads/exempleCallable/Thread002.java
+++ 02_Threads/exempleCallable/Thread002.java
@@ -2,6 +2,16 @@ package exempleCallable;
 
 import java.util.concurrent.*;
 
+/**
+ * Exemple de multithreading amb objecte Callable i un Future.
+ *
+ * Es llença un objecte Callable des d'un ExecutorService amb un sol thread
+ *  que realitza una suma de molts nombre en background.
+ *  Mentres es realitza la suma, el thread principal mostra "." punts mentres
+ *  s'esta executant la suma.
+ *  Quan acaba el calcul obtenim el resultat a traves de l'objecte Future.get()
+ *
+ */
 public class Thread002 {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ThreadReturn task = new ThreadReturn();
@@ -28,6 +38,12 @@ public class Thread002 {
     }
 }
 
+/**
+ * Objecte callable que implementa el mètode call() que realitza
+ * el calcul de la suma i retorna un valor.
+ *
+ * Abans d'executar s'ha d'haver inicialitzat el amb setNum()
+ */
 class ThreadReturn implements Callable<Long> {
 
     private long max_n;
@@ -38,6 +54,7 @@ class ThreadReturn implements Callable<Long> {
     }
     @Override
     public Long call() throws Exception {
+
         for (int i = 0; i < this.max_n; i+=2) {
             sum += i;
         }
